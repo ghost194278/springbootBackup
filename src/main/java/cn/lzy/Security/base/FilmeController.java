d54bbfc9041279cb640bec3fcebb1ce0bc0efd2e
ghost194278 <329511768@qq.com>
2023-10-16 18:50:20 +0800

    7.5 --修改用户名功能

--- src/main/java/cn/lzy/Security/base/FilmeController.java
+++ src/main/java/cn/lzy/Security/base/FilmeController.java
@@ -1,8 +1,13 @@
 package cn.lzy.Security.base;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author 江梅铭
@@ -19,6 +24,19 @@ public class FilmeController {
         String value = "detail/" + type + "/" + path;
         System.out.println(TAG + "===toDetail===" + value);
         return value;
+    }
+    @GetMapping("/userLogin")
+    public String toLoginPage(){
+        return "login/login";
+    }
+    @GetMapping("/getuserByContext")
+    @ResponseBody
+    public String getUser2() {
+        SecurityContext context = SecurityContextHolder.getContext();
+        Authentication authentication = context.getAuthentication();
+        UserDetails principal = (UserDetails)authentication.getPrincipal();
+        System.out.println("username: "+principal.getUsername());
+        return principal.getUsername();
     }
 }
 
--- src/main/java/cn/lzy/Security/redis/CustomerRepository.java
+++ src/main/java/cn/lzy/Security/redis/CustomerRepository.java
@@ -1,5 +1,9 @@
 package cn.lzy.Security.redis;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.transaction.Transactional;
 
 /**
  * @author 江梅铭
@@ -7,4 +11,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
     Customer findByUsername(String username);
+    @Transactional
+    @Modifying
+    @Query("UPDATE t_customer c SET c.username = ?1 WHERE  c.username = ?2")
+    void updateByUsername(String name,String username);
 }
