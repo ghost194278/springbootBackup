73304fbddca744c693a0bdaa620fd0b2b36bd00f
ghost194278 <329511768@qq.com>
2023-10-25 19:26:49 +0800

    9.2 定时任务实现优化

--- src/main/java/cn/lzy/ChapterApplication.java
+++ src/main/java/cn/lzy/ChapterApplication.java
@@ -7,11 +7,15 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ServletComponentScan
+@EnableAsync
 @EnableCaching
 @ImportResource("classpath:xmlproperties.xml")
+@EnableScheduling
 public class ChapterApplication extends SpringBootServletInitializer{
     public static void main(String[] args) {
         SpringApplication.run(ChapterApplication.class, args);
